3d0ad20cdfcd6911db7f46542239fac0097e8ee1
sebas-arl <sebastian.arcos2010@gmail.com>
2026-03-07 21:50:11 -0500

    chore: added .gitignore and cleaned up repository structure

--- Webinar14/my_script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print(f'My working directory is {os.getcwd()}')
-
-x = 10
-if x%2==0:
-                   print(f'{x} is an even number')
-else:
-          print(f'{x} is an odd number')
